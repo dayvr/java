976be74c00b41a86b64c76425832a9b1f9a8e3e8
Day Vazquez <dayvazquez@protonmail.com>
2022-09-20 11:55:42 +0200

    Update user.java

--- user.java
+++ user.java
@@ -1,8 +1,9 @@
+package main;
 /* Creating an user object.
    The constructor initialize the nameOfUser field. 
    Then displays the value on the screen and underlines it with dashes. */
 
-public class User {
+class User {
    // Constructor
    public User(String name) {
       String nameOfUser = name;
@@ -16,7 +17,9 @@ public class User {
       System.out.println("\n" + nameOfUser);
       System.out.println ("---------------------------");
    }
-   
+}
+
+public class Main {
    public static void main(String args[]) {
       // Using the first constructor to instantiate user1 object.
       User user1 = new User("Day");
